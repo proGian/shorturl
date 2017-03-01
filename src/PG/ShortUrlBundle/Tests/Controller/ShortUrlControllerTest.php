e405583bf65c901befb7442af53f3070d2ec8609
Gianni Prosperi <gianni.prosperi@differentweb.it>
2017-03-01 12:19:21 +0100

    Aggiunta dei primi test

---
+++ src/PG/ShortUrlBundle/Tests/Controller/ShortUrlControllerTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace PG\ShortUrlBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+/**
+ * Description of ShortUrlControllerTest
+ *
+ * @author Gianni Prosperi
+ */
+class ShortUrlControllerTest extends WebTestCase {
+    
+    public function testUnexistentUrl() {
+         $client = static::createClient();
+         
+         $crawler = $client->request('GET', '/unexistenturl');
+         
+         $this->assertEquals(200, $client->getResponse()->getStatusCode());     
+         $this->assertContains('url not found', $crawler->filter('h1.url-not-found')->text());
+    }
+    
+    public function testExistentUrl() {
+         $client = static::createClient();
+         
+         $client->request('GET', '/J213');
+         
+         $this->assertEquals(301, $client->getResponse()->getStatusCode());              
+    }
+}
